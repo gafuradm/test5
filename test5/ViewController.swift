16896ee43221c4f3c33d624bc3b72bae00e7af50
Damiruly Gafur <sholpandaniel@yandex.ru>
2023-06-21 18:40:13 +0400

    main

--- test5/ViewController.swift
+++ test5/ViewController.swift
@@ -1,18 +1,41 @@
-//
-//  ViewController.swift
-//  test5
-//
-//  Created by Aleigh Dinara on 21.06.2023.
-//
-
 import UIKit
-
 class ViewController: UIViewController {
+    let firstView: UIView = {
+       let view = UIView()
+        view.backgroundColor = .systemIndigo
+        view.layer.cornerRadius = 24
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    let secondView: UIView = {
+       let view = UIView()
+        view.backgroundColor = .systemOrange
+        view.layer.cornerRadius = 50
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "square.and.arrow.up")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    let label: UILabel = {
+        let label = UILabel()
+        label.text = "lorem ipsum dolor sit amet"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    let secondLabel: UILabel = {
+        let label = UILabel()
+        label.text = "lorem ipsum dolor sit amet"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     let button: UIButton = {
         let button = UIButton()
-        button.setTitle("Tap Me", for: .normal)
+        button.setTitle("logout", for: .normal)
         button.configuration = .filled()
-        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -24,20 +47,43 @@ class ViewController: UIViewController {
 }
 private extension ViewController {
     func setupScene() {
+        view.backgroundColor = .systemGray
         view.addSubview(button)
+        view.addSubview(firstView)
+        view.addSubview(secondView)
+        view.addSubview(imageView)
+        view.addSubview(label)
+        view.addSubview(secondLabel)
     }
-    
     func makeConstraints() {
         button.translatesAutoresizingMaskIntoConstraints = false
-
+        view.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            button.topAnchor.constraint(equalTo: view.topAnchor, constant: 710),
+            button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
+            button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            button.heightAnchor.constraint(equalToConstant: 35),
+            firstView.topAnchor.constraint(equalTo: view.topAnchor, constant: -50),
+            firstView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -555),
+            firstView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            firstView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            secondView.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+            secondView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -600),
+            secondView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 146.67),
+            secondView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -146.33),
+            imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 333),
+            imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -450),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -312),
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 333),
+            label.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -444),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            secondLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 400),
+            secondLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -375),
+            secondLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            secondLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0)
         ])
-    }
-}
-@objc private extension ViewController {
-    func buttonTapped() {
-        print("Button was tapped.")
     }
 }
